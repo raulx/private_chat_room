db3bb9c5d39003ccf520a48987258f5fd1e5be70
Raulx103 <raulx103@gmail.com>
2024-01-03 19:18:14 +0530

    added frontend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
-});
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("api is running.");
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is listening at PORT:${PORT}`);
 });
