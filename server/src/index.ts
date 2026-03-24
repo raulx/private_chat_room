337952d2ebe50841d5bafa78f63cdaef80c71d24
Raulx103 <raulx103@gmail.com>
2026-03-24 16:57:19 +0530

    feat: prettier added to server.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
-import express from "express";
-import path from "path";
-import { createServer } from "node:http";
-import { Server } from "socket.io";
+import express from 'express';
+import path from 'path';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 const app = express();
 
@@ -11,44 +11,44 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3300"],
+    origin: ['http://localhost:3300'],
   },
 });
 
-io.on("connection", (socket) => {
-  socket.on("custom-room", (room, type, callback) => {
+io.on('connection', (socket) => {
+  socket.on('custom-room', (room, type, callback) => {
     const roomExits = io.sockets.adapter.rooms.has(room);
-    if (roomExits && type === "create") {
-      callback("exits", room);
-    } else if (!roomExits && type === "join") {
-      callback("notexist");
-    } else if (roomExits && type === "leave-room") {
+    if (roomExits && type === 'create') {
+      callback('exits', room);
+    } else if (!roomExits && type === 'join') {
+      callback('notexist');
+    } else if (roomExits && type === 'leave-room') {
       socket.leave(room);
       const roomSize = io.sockets.adapter.rooms.get(room)?.size || 0;
-      io.to(room).emit("room-size", roomSize);
+      io.to(room).emit('room-size', roomSize);
       callback(room);
     } else if (room) {
       socket.join(room);
       const roomSize = io.sockets.adapter.rooms.get(room)?.size || 0;
-      io.to(room).emit("room-size", roomSize);
-      socket.on("new-chat", (data) => {
-        socket.to(room).emit("chat-broadcast", data);
+      io.to(room).emit('room-size', roomSize);
+      socket.on('new-chat', (data) => {
+        socket.to(room).emit('chat-broadcast', data);
       });
     }
   });
 });
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve("..");
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve('..');
 
-  app.use(express.static(path.join(__dirname, "client", "dist")));
+  app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
   app.use((_req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   });
 } else {
-  app.get("/", (_req, res) => {
-    res.send("api is running.");
+  app.get('/', (_req, res) => {
+    res.send('api is running.');
   });
 }
 
